98bf86696682b87586efe8ed9a8c9f3193f658cc
lroxanne <123195819+lroxanne@users.noreply.github.com>
2024-04-26 19:31:47 -0400

    add image

--- app/src/main/java/com/example/thenotesapp/fragments/EditNoteFragment.kt
+++ app/src/main/java/com/example/thenotesapp/fragments/EditNoteFragment.kt
@@ -6,35 +6,52 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.Typeface
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.provider.MediaStore
+import android.provider.Settings
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.example.thenotesapp.MainActivity
 import com.example.thenotesapp.R
 import com.example.thenotesapp.databinding.FragmentEditNoteBinding
 import com.example.thenotesapp.model.Note
 import com.example.thenotesapp.viewmodel.NoteViewModel
+import com.example.thenotesapp.viewmodel.PermissionViewModel
+import com.example.thenotesapp.viewmodel.PermissionViewModel.Companion.STORAGE_PERM_REQ_CODE_BELOW_13
+import com.example.thenotesapp.viewmodel.PermissionViewModel.Companion.STORAGE_PERM_REQ_CODE_ON_13
 import com.google.android.gms.auth.api.Auth
-import com.google.android.gms.drive.Drive
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.drive.Drive
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
 import com.google.api.client.http.ByteArrayContent
 import com.google.api.client.http.javanet.NetHttpTransport
@@ -46,6 +63,7 @@ import com.itextpdf.kernel.pdf.PdfDocument
 import com.itextpdf.kernel.pdf.PdfWriter
 import com.itextpdf.layout.Document
 import com.itextpdf.layout.element.Paragraph
+import io.github.muddz.styleabletoast.StyleableToast
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -61,10 +79,18 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
     private lateinit var notesViewModel: NoteViewModel
     private lateinit var currentNote: Note
 
+    private var shouldGoToStorageSettings = false
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var driveService: com.google.api.services.drive.Drive
     private lateinit var savedGoogleSignInAccount: GoogleSignInAccount
     private lateinit var sharedPreferences: SharedPreferences
+    private val permissionViewModel: PermissionViewModel by activityViewModels()
+    private var openImageLauncher: ActivityResultLauncher<Intent>? = null
+    private var selectedImageUri: Uri? = null
+
+    private val imageLoaderOptions = RequestOptions()
+        .diskCacheStrategy(DiskCacheStrategy.NONE) // Disable disk caching to prevent loading old images
+        .skipMemoryCache(true)
 
     private val args: EditNoteFragmentArgs by navArgs()
 
@@ -79,7 +105,8 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        sharedPreferences = requireActivity().getSharedPreferences("NoteAppPrefs",
+        sharedPreferences = requireActivity().getSharedPreferences(
+            "NoteAppPrefs",
             Context.MODE_PRIVATE
         )
 
@@ -91,26 +118,89 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
 
         binding.editNoteTitle.setText(currentNote.noteTitle)
         binding.editNoteDesc.setText(currentNote.noteDesc)
+        Glide.with(this)
+            .load(currentNote.imageUri)
+            .apply(imageLoaderOptions)
+            .into(binding.editNoteImg)
+        binding.editNoteImg.visibility = View.VISIBLE
 
         binding.editNoteFab.setOnClickListener {
             val noteTitle = binding.editNoteTitle.text.toString().trim()
             val noteDesc = binding.editNoteDesc.text.toString().trim()
 
-            if (noteTitle.isNotEmpty()){
-                val note = Note(currentNote.id, noteTitle, noteDesc, false)
+            if (noteTitle.isNotEmpty()) {
+                val note =
+                    Note(currentNote.id, noteTitle, noteDesc, false, selectedImageUri?.toString())
                 notesViewModel.updateNote(note)
                 view.findNavController().popBackStack(R.id.homeFragment, false)
             } else {
                 Toast.makeText(context, " Please enter note title", Toast.LENGTH_SHORT).show()
             }
         }
+
+        binding.addImgBtn.setOnClickListener {
+            if (permissionViewModel.checkImagePermission()) {
+                try {
+                    openImageLauncher?.launch(Intent(Intent.ACTION_PICK).apply {
+                        type = "image/*"
+                    })
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            } else {
+                permissionViewModel.askForImagePermission(this)
+            }
+        }
+
+        binding.addFontBtn.setOnClickListener {
+            showFontSelectionDialog(
+                requireContext(),
+                binding.editNoteTitle,
+                binding.editNoteHeading
+            )
+        }
+
+        openImageLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val imageUri = result.data?.data
+                    imageUri?.let {
+                        selectedImageUri = it // Store the selected image URI
+                        Glide.with(this)
+                            .load(imageUri)
+                            .apply(imageLoaderOptions)
+                            .into(binding.editNoteImg)
+                    }
+                }
+            }
     }
 
-    private fun deleteNote(){
+    fun showFontSelectionDialog(context: Context, title: TextView, heading:TextView) {
+        val fontOptions = arrayOf("Jersey", "Roboto")
+        val fontFamilies = arrayOf("jersey.ttf", "roboto.ttf")
+
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Select Font")
+
+        builder.setItems(fontOptions) { _, which ->
+            try {
+                val fontPath = "fonts/${fontFamilies.getOrElse(which) { "" }}"
+                val typeface = Typeface.createFromAsset(context.assets, fontPath)
+                title.typeface = typeface
+                heading.typeface = typeface
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        builder.show()
+    }
+
+    private fun deleteNote() {
         AlertDialog.Builder(activity).apply {
             setTitle("Delete Note")
             setMessage("Do you want to delete this note?")
-            setPositiveButton("Delete"){_,_ ->
+            setPositiveButton("Delete") { _, _ ->
                 notesViewModel.deleteNote(currentNote)
                 Toast.makeText(context, "Note Deleted", Toast.LENGTH_SHORT).show()
                 view?.findNavController()?.popBackStack(R.id.homeFragment, false)
@@ -122,41 +212,47 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
         menu.clear()
 
-        if(currentNote.isDeleted){
+        if (currentNote.isDeleted) {
             menuInflater.inflate(R.menu.menu_edit_delete_note, menu)
-        }else{
+        } else {
             menuInflater.inflate(R.menu.menu_edit_note, menu)
         }
     }
 
     override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        return when(menuItem.itemId){
+        return when (menuItem.itemId) {
 
             R.id.deleteMenu -> {
                 deleteNote()
                 true
             }
 
-            R.id.delete_note->{
+            R.id.delete_note -> {
                 thoroughDeleteNote()
                 true
             }
 
-            R.id.recover->{
+            R.id.recover -> {
                 recoverNote()
                 true
             }
 
-            R.id.uploadMenu ->{
-                if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            R.id.uploadMenu -> {
+                if (ContextCompat.checkSelfPermission(
+                        requireContext(),
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                        requireContext(),
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
                     // requestStoragePermissionForUpload()
 
                     setupGoogleSignIn()
-                }
-                else{
+                } else {
                     setupGoogleSignIn()
                 }
-               true
+                true
             }
 
             R.id.CalMenu -> {
@@ -171,8 +267,15 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
     private fun calculateAndShowWordCount() {
         val noteTitle = binding.editNoteTitle.text.toString().trim()
         val noteDesc = binding.editNoteDesc.text.toString().trim()
-        val totalWords = (noteTitle.split("\\s+".toRegex()).size + noteDesc.split("\\s+".toRegex()).size).coerceAtLeast(0)
-        Toast.makeText(context, "Total word count: $totalWords", Toast.LENGTH_LONG).show()
+        val totalWords =
+            (noteTitle.split("\\s+".toRegex()).size + noteDesc.split("\\s+".toRegex()).size).coerceAtLeast(
+                0
+            )
+        StyleableToast.Builder(binding.root.context).textSize(16f)
+            .text("Total word count: $totalWords")
+            .backgroundColor(ContextCompat.getColor(binding.root.context, R.color.red))
+            .solidBackground()
+            .textColor(Color.WHITE).cornerRadius(10).show()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -195,12 +298,16 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
                 setupGoogleSignIn()
             } else {
                 // Storage permission not granted, show a message or handle accordingly
-                Toast.makeText(requireContext(), "Storage permission not granted", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    requireContext(),
+                    "Storage permission not granted",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
 
-    private fun  thoroughDeleteNote(){
+    private fun thoroughDeleteNote() {
         AlertDialog.Builder(activity).apply {
             setTitle("Delete Note")
             setMessage("Do you want to delete this note?")
@@ -213,7 +320,7 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
         }.create().show()
     }
 
-    private fun recoverNote(){
+    private fun recoverNote() {
         notesViewModel.recoverNote(currentNote)
         Toast.makeText(context, " Note Recover", Toast.LENGTH_SHORT).show()
         view?.findNavController()?.popBackStack(R.id.trashNoteFragment, false)
@@ -225,6 +332,7 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
         editor.putString("GoogleSignInAccount", accountJson)
         editor.apply()
     }
+
     private fun setupGoogleSignIn() {
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
@@ -235,6 +343,7 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
 
         checkSavedGoogleSignInAccount()
     }
+
     private fun checkSavedGoogleSignInAccount() {
         val savedAccountJson = sharedPreferences.getString("GoogleSignInAccount", null)
         savedAccountJson?.let {
@@ -244,28 +353,37 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
             createAndUploadPdf()
         } ?: signIn() // If no saved account, initiate sign-in process
     }
+
     private fun signIn() {
         startActivityForResult(googleSignInClient.signInIntent, RC_SIGN_IN_UPLOAD)
     }
+
     private fun initializeDriveService(account: GoogleSignInAccount) {
-        val credential = GoogleAccountCredential.usingOAuth2(requireContext(), setOf(DriveScopes.DRIVE_FILE))
+        val credential =
+            GoogleAccountCredential.usingOAuth2(requireContext(), setOf(DriveScopes.DRIVE_FILE))
         credential.selectedAccount = account.account
 
         val httpTransport = NetHttpTransport()
         val jsonFactory = GsonFactory.getDefaultInstance()
 
-        driveService = com.google.api.services.drive.Drive.Builder(httpTransport, jsonFactory, credential)
-            .setApplicationName("NoteApp")
-            .build()
+        driveService =
+            com.google.api.services.drive.Drive.Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName("NoteApp")
+                .build()
     }
+
     @OptIn(DelicateCoroutinesApi::class)
     private fun createAndUploadPdf() {
         val noteTitle = binding.editNoteTitle.text.toString().trim()
         val noteDesc = binding.editNoteDesc.text.toString().trim()
 
         if (noteTitle.isNotEmpty()) {
-            val pdfFileName = "${currentNote.id}_$noteTitle.pdf" // Use note ID and title for filename
-            val pdfFilePath = File(requireContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), pdfFileName)
+            val pdfFileName =
+                "${currentNote.id}_$noteTitle.pdf" // Use note ID and title for filename
+            val pdfFilePath = File(
+                requireContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),
+                pdfFileName
+            )
 
             GlobalScope.launch(Dispatchers.IO) {
                 val success = createPdf(pdfFilePath, noteTitle, noteDesc)
@@ -348,6 +466,8 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
             document.add(Paragraph(title))
             document.add(Paragraph(description))
 
+
+
             document.close()
             outputStream.close()
 
@@ -363,6 +483,39 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
         editNoteBinding = null
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            STORAGE_PERM_REQ_CODE_ON_13, STORAGE_PERM_REQ_CODE_BELOW_13 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        openImageLauncher?.launch(
+                            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        )
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                } else {
+                    if (shouldGoToStorageSettings) {
+                        try {
+                            startActivity(Intent().apply {
+                                action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                                data = Uri.fromParts("package", requireActivity().packageName, null)
+                            })
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                    shouldGoToStorageSettings = true
+                }
+            }
+        }
+    }
+
     private fun requestStoragePermissionForUpload() {
         ActivityCompat.requestPermissions(
             requireActivity(),
@@ -373,6 +526,7 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
             STORAGE_PERMISSION_REQUEST_CODE
         )
     }
+
     companion object {
         private const val STORAGE_PERMISSION_REQUEST_CODE = 100
         private const val RC_SIGN_IN_UPLOAD = 1002
