d0a7fc1b7076de7170f99cccda5fc47ce27a9b9a
davidyh3 <yuhan59rz@gmail.com>
2024-04-28 18:43:44 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/thenotesapp/MainActivity.kt
+++ app/src/main/java/com/example/thenotesapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.thenotesapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.thenotesapp.database.NoteDatabase
+import com.example.thenotesapp.language.MultiLanguageUtil
 import com.example.thenotesapp.repository.NoteRepository
 import com.example.thenotesapp.viewmodel.NoteViewModel
 import com.example.thenotesapp.viewmodel.NoteViewModelFactory
@@ -19,7 +21,14 @@ class MainActivity : AppCompatActivity() {
         setupViewModel()
     }
 
-    private fun setupViewModel(){
+    override fun attachBaseContext(newBase: Context?) {
+        newBase?.let {
+            super.attachBaseContext(MultiLanguageUtil.INSTANCE.attachBaseContext(it))
+            MultiLanguageUtil.INSTANCE.setConfiguration(it)
+        }
+    }
+
+    private fun setupViewModel() {
         val noteRepository = NoteRepository(NoteDatabase(this))
         val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
         noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
