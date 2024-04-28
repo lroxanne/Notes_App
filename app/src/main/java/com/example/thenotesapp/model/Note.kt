fc51575751c1d9974c78c54983efbb963edb08d1
davidyh3 <yuhan59rz@gmail.com>
2024-04-28 18:47:30 -0400

    Update Note.kt

--- app/src/main/java/com/example/thenotesapp/model/Note.kt
+++ app/src/main/java/com/example/thenotesapp/model/Note.kt
@@ -1,6 +1,7 @@
 package com.example.thenotesapp.model
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
@@ -14,4 +15,15 @@ data class Note(
     val noteDesc: String,
     val isDeleted: Boolean,
     val imageUri: String?
-): Parcelable
+) : Parcelable {
+
+    @ColumnInfo(name = "year")
+    var year: Int = 0
+
+    @ColumnInfo(name = "month")
+    var month: Int = 0
+
+    @ColumnInfo(name = "day")
+    var day: Int = 0
+
+}
